aa7b0b226d3aa8bf2650fd865f5c3ce5d80f9583
mirazahmeed <mirazahmed0619@gmail.com>
2024-12-12 22:36:13 +0600

    Country code added

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Country from "../Country/Country";
+import "./Countries.css";
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
@@ -12,9 +13,11 @@ const Countries = () => {
     return (
         <div>
             <h2>Countries : {countries.length}</h2>
-            {countries.map((country) => (
-                <Country key={country.cca3} country={country}></Country>
-            ))}
+            <div className="country-container">
+                {countries.map((country) => (
+                    <Country key={country.cca3} country={country}></Country>
+                ))}
+            </div>
         </div>
     );
 };
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -2,11 +2,16 @@ import "./Country.css";
 
 const Country = ({ country }) => {
     console.log(country);
-    const { name, flags } = country;
+    const { name, flags, area, capital, borders, languages, population, cca3 } =
+        country;
     return (
         <div className="country">
             <h3>Name: {name?.common}</h3>
             <img className="flag" src={flags?.png} alt={name?.common} />
+            <p>Area: {area}</p>
+            <p>Capital: {capital}</p>
+            <h4>Population: {population}</h4>
+            <p>code: {cca3}</p>
         </div>
     );
 };
