463c82e2a51a08af6234ddd0668a658354897a2a
mirazahmeed <mirazahmed0619@gmail.com>
2024-12-13 02:11:37 +0600

    visited button and styles updated

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -4,18 +4,23 @@ import "./Countries.css";
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
+    const [visitedCountries, setVisitedCountries] = useState([]);
 
     useEffect(() => {
         fetch("https://restcountries.com/v3.1/all")
             .then((res) => res.json())
             .then((data) => setCountries(data));
     }, []);
+    
+    const handleVisitedCountry = (country) => {
+        
+    }
     return (
         <div>
             <h2>Countries : {countries.length}</h2>
             <div className="country-container">
                 {countries.map((country) => (
-                    <Country key={country.cca3} country={country}></Country>
+                    <Country handleVisitedCountry={handleVisitedCountry} key={country.cca3} country={country}></Country>
                 ))}
             </div>
         </div>
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,17 +1,31 @@
+import { useState } from "react";
 import "./Country.css";
 
-const Country = ({ country }) => {
+const Country = ({ country, handleVisitedCountry }) => {
     console.log(country);
     const { name, flags, area, capital, borders, languages, population, cca3 } =
         country;
+
+    const [visited, setVisited] = useState(false);
+    const handleVisited = () => {
+        setVisited(!visited);
+    };
     return (
-        <div className="country">
+        <div className={`country ${visited && `visited`}`}>
             <h3>Name: {name?.common}</h3>
             <img className="flag" src={flags?.png} alt={name?.common} />
             <p>Area: {area}</p>
             <p>Capital: {capital}</p>
             <h4>Population: {population}</h4>
             <p>code: {cca3}</p>
+            <button onClick={handleVisited}>
+                {visited ? "Visited" : "Going"}
+            </button>
+            <button onClick={handleVisitedCountry}>Set Visited</button>
+            {/* {visited && <div>You have visited this country</div>} */}
+            {visited
+                ? "You have visited this country"
+                : "I want to visit this country"}
         </div>
     );
 };
